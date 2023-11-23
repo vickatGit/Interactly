3975dd3d7cbc958e6aad68a1180833ba4a8468be
Vikas Gangishetty <gangishettyvikas@gmail.com>
2023-11-23 22:36:53 +0530

    enve variable placed correctly

--- src/config/config.ts
+++ src/config/config.ts
@@ -11,5 +11,8 @@ export const dbPool = mysql.createPool({
     host:process.env.DB_HOST,
     user:process.env.DB_USERNAME,
     password:process.env.DB_PASSWORD,
-    database:process.env.DB_NAME
+    database:process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10, // Adjust the limit according to your needs
+    queueLimit: 0
 }).promise()
