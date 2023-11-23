9ada6ceb425404d403049171db25f3311828460c
Vikas Gangishetty <gangishettyvikas@gmail.com>
2023-11-23 23:17:02 +0530

    env

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,15 +7,14 @@ export const crmHeaders={
     'Authorization':`Token token=${process.env.FRESHWORKS_API_KEY}`
 }
 
-// export const dbPool = mysql.createPool({
-//     host:process.env.DB_HOST,
-//     user:process.env.DB_USERNAME,
-//     password:process.env.DB_PASSWORD,
-//     database:process.env.DB_NAME,
-//     waitForConnections: true,
-//     connectionLimit: 10, // Adjust the limit according to your needs
-//     queueLimit: 0,
-//     port:process.env.DB_PORT
-// }).promise()
+export const dbPool = mysql.createPool({
+    host:process.env.DB_HOST,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined,
+    user:process.env.DB_USERNAME,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10, // Adjust the limit according to your needs
+    queueLimit: 0,
 
-export const dbPool = mysql.createPool(process.env.DB_CONNECTION_STRING)
+}).promise()
